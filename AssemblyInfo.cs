c299395aac396623e24c66d0619ace4091f7fe08
Kostya Mikhailyuk <skivsoft@gmail.com>
2015-03-05 12:35:13 +0600

    AssemblyInfo moved

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -2,8 +2,8 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersionAttribute("1.0")]
-[assembly: AssemblyFileVersionAttribute("1.0")]
+[assembly: AssemblyVersionAttribute("1.1.0")]
+[assembly: AssemblyFileVersionAttribute("1.1.0")]
 
 [assembly: AssemblyTitle("LanExchange.Network")]
 [assembly: AssemblyDescription("Managed Windows network api wrapper.")]
